ab168df85f29338264ea9cbeaac6e806b2b1e213
Nisrine Azzaim <azzaimnisrine@gmail.com>
2023-01-02 23:14:52 +0100

    first commit

---
+++ ModelTrainingApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using ModelTrainingApp;
+// Add input data
+var sampleData = new SentimentModel.ModelInput()
+{
+    New_reviews = "hotel"
+};
+
+// Load model and predict output of sample data
+var result = SentimentModel.Predict(sampleData);
+
+
+String sentiment = null; ;
+if (result.PredictedLabel == -1) sentiment = "negative";
+if (result.PredictedLabel == 0) sentiment = "neutre";
+if (result.PredictedLabel == 1) sentiment = "positive";
+Console.WriteLine($"Text: {sampleData.New_reviews}\nSentiment: {sentiment}");
